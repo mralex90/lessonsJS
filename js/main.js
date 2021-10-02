fcd2a2b95793c5de0476d43b5ad634946fbbdfef
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-03 00:03:32 +0300

    number and string

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 var num_1 = 5.4;
 var num_2 = 15;
 
-
 var num_3 = 5;
 num_3++;
 
 console.log("Result: " + num_3);
+
+var str_1 = Number("12");
+var str_2 = Number("2");
+console.log("Result: " + (str_1 + str_2));
