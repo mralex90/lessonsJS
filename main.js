05afb6e347676114fb2bc17cbbf1f3fecf2c4db9
mralex90 <71518300+mralex90@users.noreply.github.com>
2021-10-02 22:47:31 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+let yourGreatMinus = document.querySelector(".minus")
+    yourGreatMinus.innerText = 15 - 2
+    
+    let myHeader = document.querySelector(".header")
+    myHeader.innerText = "Hello"
+
+    let myName = document.querySelector(".name")
+    myName.innerText = "Oleksandr"
+    
+    let yourPlus = document.querySelector(".plus")
+    innerText = 21 + 41
+    
+    let numberOne = 10
+    let numberTwo = 15
+    let mySum = numberOne + numberTwo
+
+    let sumElement = document.querySelector(".sum")
+    sumElement.innerText = mySum
+
+    let multElement = document.querySelector(".multiply")
+    multElement.innerText = 5 * 10
