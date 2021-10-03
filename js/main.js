dd65eba0473bcdbbd8ed654d2d192b6743943b48
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-03 23:49:17 +0300

    cycle FOR

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-var arr = [5, true, "stroka", 5.7, 0, -100];
-arr[3] = "word";
-console.log(arr.length);
-
-var matrix = [
-    [4, 6, 8], ["stroka", 5.7], [0, -100]
-];
-
-matrix[1] [0] = 90;
-console.log(matrix);
+for(var i = 100; i > 5; i /= 2) {
+    console.log(i);
+}
