badc349e575e6c86ee48801a5b51be08d9bc8a35
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-03 23:09:07 +0300

    || &&

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 var number = 15;
+var isHasHouse = true;
 
-if(number != 15) {
+if(number == 15 && !isHasHouse) {
     console.log("OK");
-
 } else if(number < 10) {
     console.log("OK!!");
 } else if(number == 7) {
     console.log("7!");
-} else if(number >= 15) {
-    console.log(">= 15!");
+} else if(number > 15) {
+    console.log("> 15!");
 } else {
     console.log("Else!");
 }
