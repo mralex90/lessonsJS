3491aebe1c620c5e868f35d3e9ebf34dea5a8680
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-03 23:53:40 +0300

    cycle WHILE
    8:54

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-for(var i = 100; i > 5; i /= 2) {
-    console.log(i);
+// for(var i = 100; i > 5; i /= 2) {
+//     console.log(i);
+// }
+
+var j = 1000;
+while(j > 100) {
+    console.log(j);
+    j -= 100;
 }
