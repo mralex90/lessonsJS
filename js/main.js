2cb571edc348229882a12f8182d15a248bac4f0f
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-03 23:00:49 +0300

    else if

--- js/main.js
+++ js/main.js
@@ -2,7 +2,13 @@ var number = 15;
 
 if(number != 15) {
     console.log("OK");
+
+} else if(number < 10) {
     console.log("OK!!");
+} else if(number == 7) {
+    console.log("7!");
+} else if(number >= 15) {
+    console.log(">= 15!");
 } else {
     console.log("Else!");
 }
