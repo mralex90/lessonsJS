a490ef3daf24151c839d48c227ef0b38e3e66238
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-04 23:44:21 +0300

    prompt

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-// alert("Какая хорошая погода)");
-var data = confirm("Вы сейчас дома?");
-if(data){
-    alert("Вы молодец!");
-}
+// // alert("Какая хорошая погода)");
+// var data = confirm("Вы сейчас дома?");
+// if(data){
+//     alert("Вы молодец!");
+// }
+
+var data = prompt("Скажите сколько вам лет");
+console.log(data);
