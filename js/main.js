d06712c9015f7eda54f636f5c7d680b85c02d748
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-04 23:14:51 +0300

    do while

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
-// for(var i = 100; i > 5; i /= 2) {
-//     console.log(i);
+// // for(var i = 100; i > 5; i /= 2) {
+// //     console.log(i);
+// // }
+
+// var j = 1000;
+// while(j >= 100) {
+//     console.log(j);
+//     j -= 100;
 // }
 
-var j = 1000;
-while(j > 100) {
-    console.log(j);
-    j -= 100;
-}
+// var isHasCar = true;
+// while(isHasCar) {
+
+// }
+
+
+var x = 0;
+do {
+    console.log(x);
+    x++;
+} while(x < 50);
