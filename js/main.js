e9508e6a8146dcea71ce7e20bf3aac413822dcd9
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-05 02:15:07 +0300

    sum

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
-function info(word) {
-    console.log(word + "!");
+function summa(arr) {
+    var sum = 0;
+    for(var i = 0; i < arr.length; i++) 
+        sum += arr[i];
+
+    console.log(sum);
 }
 
-function summa(a, b) {
-    var res = a + b;
-    info(res);
-}
+var array = [6, 8, 1];
+var array_2 = [6, 8, 1, 7];
+var array_3 = [6, 8, 1, 90];
 
-summa(5, 7);
+summa(array);
+summa(array_2);
+summa(array_3);
