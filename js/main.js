a0a6580e19b03afd764e3116d77eddbaa86c5081
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-05 02:20:50 +0300

    return

--- js/main.js
+++ js/main.js
@@ -3,13 +3,15 @@ function summa(arr) {
     for(var i = 0; i < arr.length; i++) 
         sum += arr[i];
 
-    console.log(sum);
+        return sum;
 }
 
 var array = [6, 8, 1];
 var array_2 = [6, 8, 1, 7];
 var array_3 = [6, 8, 1, 90];
 
-summa(array);
-summa(array_2);
-summa(array_3);
+var res = summa(array);
+res *= 2;
+console.log("Результат: " + res);
+// summa(array_2);
+// summa(array_3);
