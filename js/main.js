cf8a1617e2eca54f889ba9cebd56c903045afe98
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-04 23:26:52 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
-// for(var i = 10; i < 20; i += 2){
-//     if(i > 15)
-//     break;
-//     console.log(i);
-// }
+var arr = [5, 7, 3, 8, 9, 91];
 
-for(var i = 10; i < 20; i ++){
-    if(i % 2 == 0)
-        continue;
-    console.log(i);
+for(var i = 0; i < arr.length; i++){
+    arr[i] *= 2;
+
+    console.log("Элемент " + (i + 1) + ": " + arr[i]);
 }
