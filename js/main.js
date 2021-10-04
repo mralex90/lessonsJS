aee4b044ec3fd14611883ecbcfc7d0cb66038819
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-04 23:36:31 +0300

    allert

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-var arr = [5, 7, 3, 8, 9, 91];
-
-for(var i = 0; i < arr.length; i++){
-    arr[i] *= 2;
-
-    console.log("Элемент " + (i + 1) + ": " + arr[i]);
-}
+// alert("Какая хорошая погода)");
+var data = confirm("Вы сейчас дома?");
+console.log(data);
