29677e1bc8832ae1a754ef1fbf987a94e6a26b64
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-04 23:52:14 +0300

    example

--- js/main.js
+++ js/main.js
@@ -4,5 +4,13 @@
 //     alert("Вы молодец!");
 // }
 
-var data = prompt("Скажите сколько вам лет");
-console.log(data);
+// var data = prompt("Скажите сколько вам лет");
+// console.log(data);
+
+var person = null;
+if(confirm("Вы точно уверены?")) {
+    person = prompt("Введите ваше имя");
+    alert("Привет, " + person);
+}else {
+    alert("Вы нажали на 'Отмена'"); 
+}
