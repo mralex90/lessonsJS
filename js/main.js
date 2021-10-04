a9d79b33aaea198bcb64cdf6acfa3623ed924f8a
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-05 01:51:17 +0300

    create simple function

--- js/main.js
+++ js/main.js
@@ -1,16 +1,6 @@
-// // alert("Какая хорошая погода)");
-// var data = confirm("Вы сейчас дома?");
-// if(data){
-//     alert("Вы молодец!");
-// }
-
-// var data = prompt("Скажите сколько вам лет");
-// console.log(data);
-
-var person = null;
-if(confirm("Вы точно уверены?")) {
-    person = prompt("Введите ваше имя");
-    alert("Привет, " + person);
-}else {
-    alert("Вы нажали на 'Отмена'"); 
+function info() {
+    console.log("Привет");
+    console.log("!");
 }
+
+info();
