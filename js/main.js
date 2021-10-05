c229f16cf800d2d2773ae906a95f75735e98c538
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-05 23:02:42 +0300

    local & global var

--- js/main.js
+++ js/main.js
@@ -1,17 +1,9 @@
-function summa(arr) {
-    var sum = 0;
-    for(var i = 0; i < arr.length; i++) 
-        sum += arr[i];
+// var num = 10;
 
-        return sum;
+function info() {
+    var num = 10;
+    console.log(num);
 }
 
-var array = [6, 8, 1];
-var array_2 = [6, 8, 1, 7];
-var array_3 = [6, 8, 1, 90];
-
-var res = summa(array);
-res *= 2;
-console.log("Результат: " + res);
-// summa(array_2);
-// summa(array_3);
+info();
+console.log(num);
