5151ad852b3755b84b526834af5558dd97b7319d
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-07 22:32:37 +0300

    work with HTML objects

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 var text = document.getElementById('text');
 text.title = "New Text";
 console.log(text.title);
+
+text.style.color = "red";
+text.style.backgroundColor = "blue";
+
+text.innerHTML = "New<br>string";
+
+document.getElementById('text').style.color = "white";
