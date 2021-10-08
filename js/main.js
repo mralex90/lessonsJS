c16aebf08f851222815de8cb2ac6bdda4ae95d04
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-08 22:57:45 +0300

    data retrieval

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 function checkForm(el) {
 
     var name = el.name.value;
-    console.log(name);
+    var pass = el.pass.value;
+    var repass = el.repass.value;
+    var state = el.state.value;
+
+    console.log(state + " - " + pass + " - " + repass);
 
     return false;
 }
