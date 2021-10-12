eaafb52495e25c1e8f3b5b022d4931a735ebc93c
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-12 23:22:30 +0300

    timer 1

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-var counter = 0;
-
-var id = setInterval(my_func, 1000);
-
-function my_func() {
-    counter++;
-    console.log("Counter: " + counter);
-
-    if(counter == 5)
-        clearInterval(id);
-}
-
-// setInterval(function() {
-    
-//     counter++;
-//     console.log("Прошло секунд: " + counter);
-// }, 1000);
+setTimeout(function() {
+    console.log("Timer is working!")
+}, 2000);
