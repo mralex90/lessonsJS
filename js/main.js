c6932ec205adc91542559329f45d47aa051613de
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-12 23:08:34 +0300

    create interval 1
    timers and intervals

--- js/main.js
+++ js/main.js
@@ -1,31 +1,7 @@
-document.getElementById('main-form').addEventListener("submit", checkForm);
+// setInterval(my_func, 1000);
 
-function checkForm(event) {
-    event.preventDefault();
-    var el = document.getElementById('main-form');
-
-    var name = el.name.value;
-    var pass = el.pass.value;
-    var repass = el.repass.value;
-    var state = el.state.value;
-
-    var fail = "";
-
-    if(name == "" || pass == "" || state == "")
-        fail = "Заполните все поля";
-    else if(name.length <= 1 || name.length > 50)
-        fail = "Введите корректное имя";
-    else if(pass != repass)
-        fail = "Пароли должны совпадать";
-    else if(pass.split("&").length > 1)
-        fail = "Некорректный пароль";
-
-    if(fail != "") 
-        document.getElementById('error').innerHTML = fail;
-
-    else {
-        alert("Все данные корректно заполнены");
-        window.location = 'https://youtube.com';
-        
-    }
-}
+// var counter = 0;
+// function my_func() {
+//     counter++;
+//     console.log("Counter: " + counter);
+// }
