0491a2c75a70562e1cea2b62016d30eae4cf93e3
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-12 23:14:15 +0300

    create interval 2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+var counter = 0;
 // setInterval(my_func, 1000);
 
 // var counter = 0;
@@ -5,3 +6,9 @@
 //     counter++;
 //     console.log("Counter: " + counter);
 // }
+
+setInterval(function() {
+    
+    counter++;
+    console.log("Прошло секунд: " + counter);
+}, 1000);
