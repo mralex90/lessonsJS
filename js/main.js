3668967c5131a780e8036eab6860aa64cf146008
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-13 21:18:58 +0300

    create class

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-var arr = [8, 90, 5, 7, 0, 8, 9];
-// console.log(arr.length);
-// console.log(arr.join(", "));
-// console.log(arr.sort());
-// console.log(arr.reverse());
+class Person {
+    constructor(name, age, happiness) {
+        this.name = name;
+        this.age = age;
+        this.happiness = happiness;
 
-var stroka = arr.reverse().join(", "); 
-
-console.log(stroka.split(", "));
+    }
+}
