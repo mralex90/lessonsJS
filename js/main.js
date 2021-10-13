13604362ccfaaccbb2bd9d61dfbc8611212a9826
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-13 21:34:06 +0300

    create object

--- js/main.js
+++ js/main.js
@@ -3,6 +3,19 @@ class Person {
         this.name = name;
         this.age = age;
         this.happiness = happiness;
+    }
 
+    info() {
+        console.log("Человек: " + this.name + ". Возраст: " + this.age);
     }
 }
+
+var alex = new Person('Alex', 30, true);
+var bob = new Person('Bob', 42, false);
+ 
+alex.name = 'alex2';
+alex.info();
+bob.info();
+
+// console.log(alex.name);
+// console.log(bob.name);
