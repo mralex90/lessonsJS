e4fcc22dd7796dc8e44038fea2e5537d39f7bb54
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-13 20:57:36 +0300

    date in JS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
-setTimeout(my_func, 2000);
-    function my_func() {
-        console.log("Timer is working!")
-}
+var date = new Date();
+
+// console.log(date.getFullYear());
+// console.log(date.getMonth() + 1);
+// console.log(date.getDate());
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getSeconds());
+
+date.setHours(23);
+date.setMinutes(23);
+
+
+console.log("Время: " + date.getHours() + ":" + date.getMinutes());
